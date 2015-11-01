86d2239e28f0530fed4f398f0dcfb80e1b61c2f4
Gopal Menon <gopal.mnn@gmail.com>
2015-11-01 22:10:12 +0000

    Fixed move commands so that robot waits till command is fully executed before returning from method call.

--- MoveRobot.py
+++ MoveRobot.py
@@ -2,6 +2,7 @@
 
 import numpy
 import gopigo
+import time
 
 import SenseLandmarks
 
@@ -24,14 +25,23 @@ ROBOT_WIDTH_CM = ROBOT_WIDTH_INCHES * INCHES_TO_CM
 ENCODER_STEPS_PER_REVOLUTION = 18
 DISTANCE_PER_ENCODER_STEP = numpy.pi * ROBOT_WHEEL_DIAMETER_CM / ENCODER_STEPS_PER_REVOLUTION
 ENCODER_STEPS_FOR_ABOUT_TURN = 24
+ENCODER_TARGET_REACHED = 0
 
 # Default speed
 DEFAULT_ROBOT_SPEED = 75
+SLEEP_TIME_BETWEEN_STEPS = 0.25
 
 #Movement uncertainty obtained from measurements
 MOVEMENT_UNCERTAINTY = numpy.matrix([[0, 0, 0],
                                      [0, 0, 0],
                                      [0, 0, 0]])
+
+# Wait till encoder target is reached
+def wait_till_encoder_target_reached():
+
+	time.sleep(SLEEP_TIME_BETWEEN_STEPS)
+	while gopigo.read_enc_status() != ENCODER_TARGET_REACHED:
+		time.sleep(SLEEP_TIME_BETWEEN_STEPS)
 
 # Go forward by a fixed amount
 def go_forward(forward_distance):
@@ -41,6 +51,7 @@ def go_forward(forward_distance):
 	gopigo.set_speed(DEFAULT_ROBOT_SPEED)
 	gopigo.enc_tgt(1, 1, encoder_steps_required)
 	gopigo.fwd()
+	wait_till_encoder_target_reached()
 
 # Go back by a fixed amount
 def go_backwards(backwards_distance):
@@ -50,6 +61,7 @@ def go_backwards(backwards_distance):
 	gopigo.set_speed(DEFAULT_ROBOT_SPEED)
 	gopigo.enc_tgt(1, 1, encoder_steps_required)
 	gopigo.bwd()
+	wait_till_encoder_target_reached()
 
 # Turn in place by the degrees specified. Negative for anti-clockwise.
 def turn_in_place(degrees_to_turn):
@@ -81,6 +93,8 @@ def turn_in_place(degrees_to_turn):
 		gopigo.right_rot()
 	else:
 		gopigo.left_rot()
+
+	wait_till_encoder_target_reached()
 
 # Go towards the nearest obstacle ahead	
 def go_towards_nearest_obstacle():
